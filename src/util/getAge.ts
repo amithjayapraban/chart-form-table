3ff13011156660faf766571a1b751a64b8419b77
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2023-05-11 13:32:02 +0530

    fixed date isuue on safari

--- src/util/getAge.ts
+++ src/util/getAge.ts
@@ -1,10 +1,12 @@
 export const getAge = (birthday: string | undefined) => {
   if (birthday) {
+    console.log((birthday))
     let [d, m, y] = birthday.split("/");
     const diff =
       Date.now() -
-      new Date(`${parseInt(y)}-${parseInt(m)}-${parseInt(d)}`).getTime();
+      new Date(`${parseInt(y)}/${parseInt(m)}/${parseInt(d)}`).getTime();
     const ageDate = new Date(diff);
+    console.log(ageDate.getUTCFullYear() - 1970);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
 };
