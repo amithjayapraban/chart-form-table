77162ceb25bdbb4613219f6b649f14a44933d07e
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2023-05-11 13:34:53 +0530

    rm logs

--- src/util/getAge.ts
+++ src/util/getAge.ts
@@ -1,12 +1,10 @@
 export const getAge = (birthday: string | undefined) => {
   if (birthday) {
-    console.log((birthday))
     let [d, m, y] = birthday.split("/");
     const diff =
       Date.now() -
       new Date(`${parseInt(y)}/${parseInt(m)}/${parseInt(d)}`).getTime();
     const ageDate = new Date(diff);
-    console.log(ageDate.getUTCFullYear() - 1970);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
 };
